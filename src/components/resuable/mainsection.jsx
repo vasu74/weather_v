4db3fbe00b0858595bc016b1cbd471fdd6e6f814
vasu74 <vasug7409@gmail.com>
2024-07-24 01:38:43 +0530

    third

--- src/components/resuable/mainsection.jsx
+++ src/components/resuable/mainsection.jsx
@@ -64,7 +64,9 @@ const MainSection = () => {
         </div>
       </div>
       {/*  Second Sidebar  */}
-      <div className="flex flex-col gap-2 w-full">{/* <BigSide /> */}</div>
+      <div className="flex flex-col gap-2 w-full">
+        <BigSide />
+      </div>
     </div>
   );
 };
